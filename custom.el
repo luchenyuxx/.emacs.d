587a3065d75d3810f89306865ac2f53384ec5057
Chenyu LU <chenyu.lu@ledger.fr>
2019-04-06 17:00:09 +0200

    init

---
+++ custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(column-number-mode t)
+ '(create-lockfiles nil)
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(display-line-numbers nil)
+ '(display-line-numbers-type nil)
+ '(display-time-mode t)
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(org-agenda-files (quote ("~/Sync/org/explorer-status-meeting-04042019.org")))
+ '(package-selected-packages
+   (quote
+    (org-present popup-imenu evil-collection magit projectile evil use-package)))
+ '(ring-bell-function (quote ignore))
+ '(scroll-bar-mode nil)
+ '(scroll-error-top-bottom nil)
+ '(show-paren-mode t)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 140)))))
---
+++ init.el
@@ -0,0 +1,94 @@
+;; global variables
+(setq
+ show-paren-delay 0.5
+ ;; install package if not installed
+ use-package-always-ensure t)
+
+(add-to-list 'exec-path "/usr/local/bin")
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+
+;; modes
+;; (electric-indent-mode 0)
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                    ("org" . "http://orgmode.org/elpa/")
+                    ("melpa" . "http://melpa.org/packages/")
+                    ("melpa-stable" . "http://stable.melpa.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; This is only needed once, near the top of the file
+(eval-when-compile (require 'use-package))
+
+(use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
+  :config)
+
+(use-package company
+  :diminish company-mode
+  :init
+  (setq
+   company-idle-delay 0))
+(add-hook 'after-init-hook 'global-company-mode)
+
+(use-package ensime
+  :pin melpa-stable
+  :init
+  (setq ensime-search-interface 'ivy)
+  (setq ensime-startup-notification nil))
+
+(use-package ivy
+  :diminish ivy-mode
+  :init
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  :config
+  (ivy-mode 1))
+(use-package counsel
+  :diminish counsel-mode
+  :config
+  (counsel-mode 1))
+(use-package swiper
+  :config)
+
+(use-package projectile
+  :pin melpa-stable
+  :init
+  (setq projectile-completion-system 'ivy)
+  :config
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode +1))
+
+(use-package magit)
+
+(use-package org-present
+  :config
+  (progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))))
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load-file custom-file)
+
+(when (fboundp 'windmove-default-keybindings) (windmove-default-keybindings))
