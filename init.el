411643e9212163e07b70adcba9c105f381d3548a
Chenyu LU <chenyu.lu@ledger.fr>
2019-05-14 11:46:07 +0200

    add sbt mode

--- init.el
+++ init.el
@@ -72,6 +72,15 @@
 (use-package magit)
 (use-package json-mode)
 (use-package scala-mode)
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
 (use-package sbt-mode)
 (use-package haskell-mode)
 (use-package dockerfile-mode)
